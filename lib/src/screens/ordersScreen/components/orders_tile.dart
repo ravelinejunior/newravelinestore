3f1ffc17c6b35c111e4c01ec78a984339f4d38f6
ravelinejunior <franklin.junior@ctw.bmwgroup.com>
2022-10-03 11:56:05 +0100

    Products List

--- lib/src/screens/ordersScreen/components/orders_tile.dart
+++ lib/src/screens/ordersScreen/components/orders_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:newravelinestore/data/model/cart_item_model.dart';
 import 'package:newravelinestore/data/model/order_model.dart';
 import 'package:newravelinestore/data/services/utils_services.dart';
 
@@ -22,6 +23,7 @@ class OrdersTile extends StatelessWidget {
             children: [
               Text(
                 'Pedido ${order.id}',
+                style: const TextStyle(fontWeight: FontWeight.bold),
               ),
               Text(
                 utilsService.formatDateTime(order.dateCreated),
@@ -33,7 +35,77 @@ class OrdersTile extends StatelessWidget {
               ),
             ],
           ),
+          childrenPadding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 5,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: ListView(
+                      physics: const BouncingScrollPhysics(),
+                      addAutomaticKeepAlives: true,
+                      addRepaintBoundaries: true,
+                      shrinkWrap: true,
+                      children: order.items
+                          .map(
+                            (orderItem) => _OrderItemMapped(
+                              utilsService: utilsService,
+                              orderItem: orderItem,
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.teal,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class _OrderItemMapped extends StatelessWidget {
+  const _OrderItemMapped({
+    Key? key,
+    required this.utilsService,
+    required this.orderItem,
+  }) : super(key: key);
+
+  final UtilsService utilsService;
+  final CartItemModel orderItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Row(
+        children: [
+          Text(
+            '${orderItem.quantity} ${orderItem.item.unit}',
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(width: 4),
+          Expanded(
+            child: Text(
+              orderItem.item.itemName,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+              overflow: TextOverflow.fade,
+            ),
+          ),
+          Text(
+            utilsService.priceToCurrencyString(orderItem.item.price),
+          ),
+        ],
       ),
     );
   }
